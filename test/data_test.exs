b06613eec5a2118cfaa8e79f0278f83ad4166650
Poncho Martinez <poncho.811@gmail.com>
2017-04-07 15:48:13 -0500

    Update test for map type station

--- test/data_test.exs
+++ test/data_test.exs
@@ -5,13 +5,13 @@ defmodule DataTest do
   @dummy_metro_file "test/metro_test"
 
   test "Format metro file" do
-    assert load_metro(@dummy_metro_file) == [["Estacion1", "1,2", "40"],
-                                        ["Estacion2", "3", "100"],
-                                        ["Estacion3", "4,5,6", "0"]]
+    assert load_metro(@dummy_metro_file) == [%{name: "Estacion1", lines: ["1", "2"], times: ["40"], order: ["1"]},
+                                        %{name: "Estacion2", lines: ["3"], times: ["100"], order: ["2"]},
+                                        %{name: "Estacion3", lines: ["4", "5", "6"], times: ["0"], order: ["3"]}]
   end
 
   test "Get and format station data" do
     assert load_metro(@dummy_metro_file) 
-            |> get_station("Estacion1") == %{name: "Estacion1", lines: ["1", "2"], time: "40"}
+            |> get_station("Estacion1") == %{name: "Estacion1", lines: ["1", "2"], times: ["40"], order: ["1"]}
   end
 end
